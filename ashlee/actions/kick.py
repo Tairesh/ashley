058f4f1575b707724252bd262bc23287f78cfe8e
Ilya Agafonov <tairesh.rus@gmail.com>
2021-02-21 00:21:35 +0500

    kick action

---
+++ ashlee/actions/kick.py
@@ -0,0 +1,50 @@
+from typing import List
+
+from telebot.apihelper import ApiException
+from telebot.types import Message, User, Chat
+
+from ashlee import emoji, constants, stickers, utils
+from ashlee.action import Action
+
+
+class Kick(Action):
+
+    def get_description(self) -> str:
+        return 'кикнуть юзера из чата'
+
+    def get_name(self) -> str:
+        return emoji.CANCEL + ' Бан'
+
+    def get_cmds(self) -> List[str]:
+        return ['kick', 'ban', 'psyop']
+
+    def get_keywords(self) -> List[str]:
+        return ['забань', 'кикни', 'забанить', 'кикнуть']
+
+    def _can_kick(self, chat: Chat, admin: User):
+        if admin.id in constants.ADMINS:
+            return True
+
+        member = self.bot.get_chat_member(chat.id, admin.id)
+        if member.can_restrict_members or member.status in {"creator", "administrator"}:
+            return True
+
+        return False
+
+    @Action.save_data
+    @Action.send_typing
+    def call(self, message: Message):
+        if not self._can_kick(message.chat, message.from_user):
+            return
+
+        if not message.reply_to_message:
+            return
+
+        try:
+            user = message.reply_to_message.from_user
+            name = 'Psy-op' if message.text.startswith('/psyop') else utils.user_name(user, True)
+            self.bot.kick_chat_member(message.chat.id, user.id)
+            self.bot.restrict_chat_member(message.chat.id, user.id)
+            self.bot.reply_to(message, f"{name} был забанен")
+        except ApiException:
+            self.bot.send_sticker(message.chat.id, stickers.CANT_DO, message.message_id)
