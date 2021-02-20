fc3eddfa9b05c56db417734ffa8d868d9286df0f
Ilya Agafonov <tairesh.rus@gmail.com>
2021-02-21 00:08:30 +0500

    restart action

---
+++ ashlee/actions/restart.py
@@ -0,0 +1,29 @@
+import os
+from typing import List
+
+from telebot.types import Message
+
+from ashlee import emoji
+from ashlee.action import Action
+
+
+class Restart(Action):
+
+    def get_description(self):
+        return None
+
+    def get_name(self) -> str:
+        return emoji.REPEAT + ' Restart'
+
+    def get_cmds(self) -> List[str]:
+        return ['restart']
+
+    def get_keywords(self) -> List[str]:
+        return []
+
+    @Action.save_data
+    @Action.send_typing
+    @Action.only_master
+    def call(self, message: Message):
+        self.bot.reply_to(message, 'OK, master')
+        os.system('sudo systemctl restart ashlee.service')
