65bfbaced30dad924155fcd5b65f824b30b80d60
Ilya Agafonov <tairesh.rus@gmail.com>
2021-02-20 19:52:32 +0500

    dog action

---
+++ ashlee/actions/dog.py
@@ -0,0 +1,32 @@
+import json
+from typing import List
+
+import requests
+from telebot.types import Message
+
+from ashlee import emoji
+from ashlee.action import Action
+
+
+class Dog(Action):
+
+    API_URL = "https://dog.ceo/api/breeds/image/random"
+
+    def get_keywords(self) -> List[str]:
+        return ['скинь собаку', 'покажи собаку']
+
+    def get_cmds(self) -> List[str]:
+        return ['dog']
+
+    def get_name(self) -> str:
+        return emoji.DOG + " Dogs"
+
+    @Action.save_data
+    @Action.send_uploading_photo
+    def call(self, message: Message):
+        data = json.loads(requests.get(self.API_URL).content.decode('utf-8'))
+        url = data['message']
+        if url.endswith('.gif'):
+            self.bot.send_video(message.chat.id, url, reply_to_message_id=message.message_id)
+        else:
+            self.bot.send_photo(message.chat.id, url, reply_to_message_id=message.message_id)
