31e8203b2695dcd3da15992ce5d33b379a45c73e
Ilya Agafonov <tairesh.rus@gmail.com>
2021-02-20 23:25:56 +0500

    forget action

---
+++ ashlee/actions/forget.py
@@ -0,0 +1,34 @@
+from typing import List
+
+from telebot.types import Message
+
+from ashlee import emoji, pepe, utils
+from ashlee.action import Action
+
+
+class Forget(Action):
+
+    def get_description(self):
+        return None
+
+    def get_name(self) -> str:
+        return emoji.CANCEL + ' Forget'
+
+    def get_cmds(self) -> List[str]:
+        return ['forget']
+
+    def get_keywords(self) -> List[str]:
+        return []
+
+    @Action.save_data
+    @Action.send_typing
+    @Action.only_master
+    def call(self, message: Message):
+        if not message.reply_to_message or not message.reply_to_message.text \
+                or message.reply_to_message.from_user.id != self.tgb.me.id:
+            return
+
+        trigrams = pepe.remove_all_trigrams(self.tgb.redis, message.reply_to_message.text)
+        text = "These keys have been successfully forgotten: " + ", ".join(['->'.join(t) for t in trigrams])
+        for t in utils.chunks(text, 3000):
+            self.bot.reply_to(message, t)
