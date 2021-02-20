e3e43ded2b0e923d75c326d7e6f264c36b4aeecc
Ilya Agafonov <tairesh.rus@gmail.com>
2021-02-20 19:50:08 +0500

    cat action

---
+++ ashlee/actions/cat.py
@@ -0,0 +1,36 @@
+import json
+from typing import List
+
+import requests
+from telebot.types import Message
+
+from ashlee import emoji
+from ashlee.action import Action
+
+
+class Cat(Action):
+
+    API_URL = "https://api.thecatapi.com/v1/images/search?mime_types={}"
+
+    def get_keywords(self) -> List[str]:
+        return ['скинь кота', 'покажи кота']
+
+    def get_cmds(self) -> List[str]:
+        return ['cat', 'catgif']
+
+    def get_name(self) -> str:
+        return emoji.CAT + " Cats"
+
+    @Action.save_data
+    @Action.send_uploading_photo
+    def call(self, message: Message):
+        if message.text and message.text.startswith('/catgif'):
+            mime_type = 'gif'
+        else:
+            mime_type = 'jpg'
+        data = json.loads(requests.get(self.API_URL.format(mime_type)).content.decode('utf-8'))[0]
+        url = data['url']
+        if url.endswith('.gif'):
+            self.bot.send_video(message.chat.id, url, reply_to_message_id=message.message_id)
+        else:
+            self.bot.send_photo(message.chat.id, url, reply_to_message_id=message.message_id)
