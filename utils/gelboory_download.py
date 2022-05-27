5eb034b486a6e07faa64f16dbef369b59cf798cd
Ilya Agafonov <tairesh.rus@gmail.com>
2022-05-27 20:37:36 +0500

    Update utils script

--- utils/gelboory_download.py
+++ utils/gelboory_download.py
@@ -1,29 +1,36 @@
 import os
-import urllib.request
+import sys
+from urllib.request import urlopen
 from xml.etree import ElementTree
 
 import requests
 
 
-WORD = 'horse'
-FOLDER = 'horse'
-API_URL = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=" + WORD
+API_URL = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags="
 
 
 if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        print('Argument not found')
+        exit()
+
+    word = sys.argv[1]
+    API_URL += word
+
     urls = []
     print("Loading gelbooru.com...")
 
-    root = ElementTree.parse(urllib.request.urlopen(API_URL)).getroot()
+    root = ElementTree.parse(urlopen(API_URL)).getroot()
     posts = root.findall('post')
     for post in posts:
-        url = post.attrib['file_url']
+        url = post.find('file_url').text
         urls.append(url)
 
     ll = len(urls)
     for i, url in enumerate(urls):
         file_name = url.split('/').pop()
-        if os.path.isfile(os.path.join('res', FOLDER, file_name)):
+        if os.path.isfile(os.path.join('res', word, file_name)):
             print(f"Skipping {i+1}/{ll} (already exists): {file_name}")
             continue
         ext = file_name.split('.').pop()
@@ -35,6 +42,6 @@ if __name__ == "__main__":
         if not response.ok:
             print(response)
         else:
-            with open(os.path.join('res', FOLDER, file_name), 'wb') as fp:
+            with open(os.path.join('res', word, file_name), 'wb') as fp:
                 fp.write(response.content)
                 print('Done!')
