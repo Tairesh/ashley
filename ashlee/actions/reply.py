9111a826d54f851a976bf8ca5dc6848d7b36640d
Ilya Agafonov <tairesh.rus@gmail.com>
2023-02-01 05:41:13 +0500

    Use davinchi again, ada is shit

--- ashlee/actions/reply.py
+++ ashlee/actions/reply.py
@@ -49,7 +49,7 @@ class Reply(Action):
         prompt += "\n\nQ: " + text + "\nA: "
         try:
             response = openai.Completion.create(
-                model="text-ada-001",
+                model="text-davinci-003",
                 prompt=prompt,
                 temperature=1,
                 max_tokens=500,
