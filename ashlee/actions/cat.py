4d8c603feac7f58fd591fb344b6ff0b3c10e3e0e
Ilia Agafonov <tairesh.rus@gmail.com>
2024-02-28 15:20:20 +0300

    :cat: Add talking /cat

--- ashlee/actions/cat.py
+++ ashlee/actions/cat.py
@@ -1,16 +1,19 @@
 import json
 from typing import List
+from urllib.parse import quote_plus
 
 import requests
 from telebot.types import Message
 
 from ashlee import emoji
 from ashlee.action import Action
+from ashlee.utils import get_keyword
 
 
 class Cat(Action):
 
     API_URL = "https://api.thecatapi.com/v1/images/search?mime_types={}"
+    API_WITH_TEXT_URL = "https://cataas.com/cat/says/{}?fontSize=70&fontColor=white"
 
     def get_description(self) -> str:
         return "случайное фото кота"
@@ -31,10 +34,14 @@ class Cat(Action):
             mime_type = "gif"
         else:
             mime_type = "jpg"
-        data = json.loads(
-            requests.get(self.API_URL.format(mime_type)).content.decode("utf-8")
-        )[0]
-        url = data["url"]
+        text = quote_plus(get_keyword(message))
+        if text:
+            url = self.API_WITH_TEXT_URL.format(text)
+        else:
+            data = json.loads(
+                requests.get(self.API_URL.format(mime_type)).content.decode("utf-8")
+            )[0]
+            url = data["url"]
         if url.endswith(".gif"):
             self.bot.send_video(
                 message.chat.id, url, reply_to_message_id=message.message_id
